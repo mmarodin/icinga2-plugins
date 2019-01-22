1d444f2a1c2c6298344600af7b49bb042a8bab6d
Morgan Marodin <morgan.marodin@pedongroup.com>
2019-01-22 12:25:18 +0100

    Added check_pgsql_deadlock.sh script to check for PostgreSQL lock

---
+++ linux/check_pgsql_deadlock.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#----------
+# Check for PostgreSQL lock
+# v.20180119 by mmarodin
+#
+HOST=127.0.0.1
+USER=icinga2
+DB=mydbname
+QUERY="SELECT blocked_locks.pid AS blocked_pid, blocked_activity.usename AS blocked_user, blocking_locks.pid AS blocking_pid, blocking_activity.usename AS blocking_user, blocked_activity.query AS blocked_statement, blocking_activity.query AS current_statement_in_blocking_process FROM  pg_catalog.pg_locks blocked_locks JOIN pg_catalog.pg_stat_activity blocked_activity ON blocked_activity.pid = blocked_locks.pid JOIN pg_catalog.pg_locks blocking_locks ON blocking_locks.locktype = blocked_locks.locktype AND blocking_locks.DATABASE IS NOT DISTINCT FROM blocked_locks.DATABASE AND blocking_locks.relation IS NOT DISTINCT FROM blocked_locks.relation AND blocking_locks.page IS NOT DISTINCT FROM blocked_locks.page AND blocking_locks.tuple IS NOT DISTINCT FROM blocked_locks.tuple AND blocking_locks.virtualxid IS NOT DISTINCT FROM blocked_locks.virtualxid AND blocking_locks.transactionid IS NOT DISTINCT FROM blocked_locks.transactionid AND blocking_locks.classid IS NOT DISTINCT FROM blocked_locks.classid AND blocking_locks.objid IS NOT DISTINCT FROM blocked_locks.objid AND blocking_locks.objsubid IS NOT DISTINCT FROM blocked_locks.objsubid AND blocking_locks.pid != blocked_locks.pid JOIN pg_catalog.pg_stat_activity blocking_activity ON blocking_activity.pid = blocking_locks.pid WHERE NOT blocked_locks.GRANTED;"
+
+RESULT=`psql -h $HOST -U $USER -d $DB -c "$QUERY" -x`
+
+  if [ "$RESULT" == "(No rows)" ] ; then
+    echo "OK - No DeadLock present"
+    EXIT=0
+  else
+    echo "CRITICAL - $RESULT"
+    EXIT=2
+  fi
+    
+exit $EXIT
